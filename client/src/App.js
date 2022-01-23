9e0724a5f75f5cf854f46cea9664adb3f912be01
roadarloceo <82858292+roadarloceo@users.noreply.github.com>
2022-01-23 14:29:10 -0800

    com

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,11 @@ function App() {
         </header>
         <div>
           <h2>Contract address</h2>
+          <div className='tokenomics'>
+            <p><strong>Buy</strong> <span>&mdash;</span> 9% marketing, 3% liquidity</p>
+            <p><strong>Sell</strong> <span>&mdash;</span> 11% marketing, 4% liquidity</p>
+            <p>1 million supply. Half burned.</p>
+          </div>
           <h4>Karate chop these paper-handed bitches.</h4>
         </div>
         <div className='logo-container'>
